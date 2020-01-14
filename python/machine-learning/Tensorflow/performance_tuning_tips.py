64f06b39cbac325dc9bb8ee5dcdf2656c3ce2cf8
Ziyan Jerry Chen <jerryc443@gmail.com>
2020-01-14 11:32:24 +0800

    minor tweak

--- python/machine-learning/Tensorflow/performance_tuning_tips.py
+++ python/machine-learning/Tensorflow/performance_tuning_tips.py
@@ -11,32 +11,33 @@ print(f'Tensorflow v{tf.__version__}'.center(90, '·'))
 
 tf_exp_config = tf.config.experimental
 
-print('\nLocal Devices:')
+print('Local Devices:')
 gpu_name_re = re.compile('name: ?([A-Za-z0-9 ]+)(?=(?:,|$))')
 cp_cap_re = re.compile('compute capability: ?([0-9.]+)(?=(?:,|$))')
 unit = ('M', 'G', 'T', 'P''E',)
 for i, x in enumerate(device_lib.list_local_devices()):
-    print(f'\t{i}: name \t=[{x.name}]')
+    print(f'\t{i}: name \t= [{x.name}]')
     try:
         _desc = x.physical_device_desc
-        print(f'\t{i}: model\t=[{gpu_name_re.search(_desc).group(1)}]')
-        print(f'\t{i}: capability\t=[{cp_cap_re.search(_desc).group(1)}]')
+        print(f'\t{i}: model\t= [{gpu_name_re.search(_desc).group(1)}]')
+        print(f'\t{i}: capablt.\t= [{cp_cap_re.search(_desc).group(1)}]')
         del _desc
     except AttributeError:
         ...
-    print(f'\t{i}: type \t=[{x.device_type}]')
+    print(f'\t{i}: type \t= [{x.device_type}]')
     _mem = x.memory_limit / 1024 / 1024
     unit_i = 0
     while _mem > 1024 and unit_i < len(unit):
         _mem /= 1024
         unit_i += 1
-    print(f'\t{i}: memory\t=[{_mem:.1f} {unit[unit_i]}]')
+    print(f'\t{i}: memory\t= [{_mem:.1f} {unit[unit_i]}iB]')
     print()
     del _mem, unit_i
 del gpu_name_re
 
 print('Visible Devices:')
-for i, x in enumerate(tf_exp_config.get_visible_devices()): print(f'\t{i}: {x}')
+for i, x in enumerate(tf_exp_config.get_visible_devices()):
+    print(f'\t{i}: {x}')
 print()
 
 print(f'Device Policy: {tf_exp_config.get_device_policy()}')
@@ -50,7 +51,7 @@ for i, x in enumerate(gpus):
     print(f'\t{i}: Memory Growth: {tf_exp_config.get_memory_growth(x)}')
 print()
 
-del tf_exp_config, unit, i, x
+del tf_exp_config, unit
 
 # %%
 from tensorflow.keras.mixed_precision import experimental as mixed_precision
@@ -61,7 +62,7 @@ for i, x in enumerate(device_lib.list_local_devices()):
     _desc = x.physical_device_desc
     _capability = float(cp_cap_re.search(_desc).group(1))
     if _capability > 7:
-        print('Enabling Mixed Precision...')
+        print('Enabling Mixed Precision'.center(90, '·'))
         float16_policy = mixed_precision.Policy('mixed_float16')
         mixed_precision.set_policy(float16_policy)
         print(f'\tCompute  dtype: {float16_policy.compute_dtype}')
@@ -72,9 +73,8 @@ for i, x in enumerate(device_lib.list_local_devices()):
     del cp_cap_re, _desc, _capability
     print()
     break
+del i, x
 
 # %%
 tf.config.optimizer.set_jit(True)
-print('Enabled XLA for TensorFlow models')
-
-# %%
+print('Enabled XLA for TensorFlow models'.center(90, '·'))
