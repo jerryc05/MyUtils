f2f5a6d7737c73cb53e186cd7fff935e9353d130
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2021-06-05 02:05:04 -0400

    apex

--- python/games/apex-legends.py
+++ python/games/apex-legends.py
@@ -21,6 +21,7 @@ nv_gpu = nv_gpu_.lower() == 'y'
 (f'''
 bind "MWHEELDOWN"           "+jump;+forward"    0
 bind "MWHEELUP"             "+jump"             0
+bind_US_standard "SPACE"    "+jump"             0
 bind_US_standard "`"        "exec autoexec"
 bind_US_standard "f"        "+ping"             0           // Ping (tap or hold)
 bind_US_standard "mouse2"   "+toggle_zoom"      0           // Toggle ADS
@@ -39,9 +40,14 @@ cl_forcepreload 1
 cl_fovScale "1.7"               // 1.55 = 110FOV | 1.7 = 120FOV
 cl_ragdoll_collide 0
 cl_showfps "4"
+closecaption "1"
+damage_indicator_style_pilot "1"
 fps_max 300
+hud_setting_anonymousMode "1"
+hud_setting_damageTextStyle "3"
 hud_setting_minimapRotate "1"   // This setting allows the minimap to rotate with you (default 0)
 hud_setting_pingAlpha "0.4"     // Opacity of the pings and downed banners (default 1)
+hud_setting_streamerMode "1"
 m_acceleration "0"              // Once again this makes sure mouse acceleration is off
 m_filter "0"                    // Makes sure any kind of mouse filtering is off
 m_rawinput "1"                  // Takes the direct input of your mouse
@@ -57,12 +63,17 @@ r_dynamic 0
 r_fastzreject "{-1 if nv_gpu else 0}"
 r_fullscreen 1
 r_particle_timescale 3
+sound_volume_dialogue "0.5"
+sound_volume_music_game "0.5"
+sound_volume_music_lobby "0.25"
 ssao_enabled "0"
 ssao_blur "0"
 ssao_radius "59"
 sprint_view_shake_style "1"     // Less Headbob
 telemetry_client_enable 0
 telemetry_client_sendInterval 0
+toggle_on_jump_to_deactivate "1"
+toggle_on_jump_to_deactivate_changed "1"
 twitch_prime_linked 1
 {"r_shadows 0" if shadow_ok else ""}
 ''')
