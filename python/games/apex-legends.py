92dbf793fab326ab58b6af9944117ffae70e1072
Ziyan "Jerry" Chen <jerryc443@gmail.com>
2021-06-04 20:14:30 -0400

    upd apex

--- python/games/apex-legends.py
+++ python/games/apex-legends.py
@@ -23,14 +23,19 @@ bind F11 "exec autoexec"
 bind "MWHEELDOWN" "+jump" 0     // Bhop
 bind "MWHEELUP"   "+jump" 0     // Bhop
 bind_US_standard "f"      "+ping"        0              // Ping (tap or hold)
-bind_US_standard "mouse2" "+zoom"   	 0              // Toggle ADS
+bind_US_standard "mouse2" "+toggle_zoom" 0              // Toggle ADS
+// bind_US_standard "mouse2" "+zoom"        0              // Hold ADS
 bind_US_standard "mouse3" "ping_specific_type ENEMY" 0  // Ping enemy
 bind_US_standard "mouse4" "+melee"       0              // Quick melee attack
 bind_US_standard "mouse5" "+offhand1"    0              // Tactical ability
 bind_US_standard "q"      "+weaponcycle" 0              // Cycle weapon
+bind_US_standard "5"      "use_consumable SHIELD_LARGE"  0  // Quick-use medpack (shield battery)
+bind_US_standard "6"      "use_consumable SHIELD_SMALL"  0  // Quick-use medpack (shield cell)
+bind_US_standard "7"      "use_consumable HEALTH_LARGE"  0  // Quick-use medpack (med kit)
+bind_US_standard "8"      "use_consumable HEALTH_SMALL"  0  // Quick-use medpack (syringe)
 chroma_enable 0                 // Disables Razer Chroma
 cl_forcepreload 1
-cl_fovScale "1.6"               // 1.55 = 110FOV | 1.7 = 120FOV
+cl_fovScale "1.7"               // 1.55 = 110FOV | 1.7 = 120FOV
 cl_ragdoll_collide 0
 cl_showfps "4"
 fps_max 300
@@ -42,7 +47,7 @@ m_rawinput "1"                  // Takes the direct input of your mouse
 mat_bloom_scalefactor_scalar 0  // Disables bloom (nothing to do with recoil, just the graphical effect you fn weirdo)
 mat_compressedtextures 1
 mat_screen_blur_enabled 0
-mouse_sensitivity "1.3"         // Your mouse sensitivity
+mouse_sensitivity "1.6"         // Your mouse sensitivity
 noise_filter_scale 0            // Removes Film-grain
 player_setting_damage_closes_deathbox_menu "0"          // Your inventory wont auto-close anymore when receiving damage
 r_dxgi_max_frame_latency 0
